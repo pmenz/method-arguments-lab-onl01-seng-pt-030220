e487931333eea21e530bcb0f959facb048dc9656
Pablo Menz <p.menz.r@gmail.com>
2020-03-11 06:45:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,8 @@
 
+def introduction_(name)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
